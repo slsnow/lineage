72844d6b81dbf6311a4a4c4156a9bed3aea1f3cf
Samuel Snow <ssnow@suse.com>
2023-04-05 20:59:31 -0600

    Update setup_postgresql.sh

--- server/setup_postgresql.sh
+++ server/setup_postgresql.sh
@@ -17,4 +17,17 @@ sudo su - postgres -c "psql -c \"CREATE USER $POSTGRES_USER WITH PASSWORD '$POST
 sudo su - postgres -c "psql -c \"CREATE DATABASE $POSTGRES_DB OWNER $POSTGRES_USER;\""
 sudo su - postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_DB TO $POSTGRES_USER;\""
 
+# Create the people table
+sudo su - postgres -c "psql -U $POSTGRES_USER -d $POSTGRES_DB -c 'CREATE TABLE people (
+    id SERIAL PRIMARY KEY,
+    gedcom_id VARCHAR(20) UNIQUE NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    gender CHAR(1),
+    birth_date DATE,
+    birth_place VARCHAR(100),
+    death_date DATE,
+    death_place VARCHAR(100)
+);'"
+
 echo "PostgreSQL setup is complete."
