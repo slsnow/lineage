5d1ba012610334ebe7d8a85c865c2cb342b3320d
Samuel Snow <ssnow@suse.com>
2023-04-06 20:17:41 -0600

    Create lineage_setup.sh

---
+++ server/lineage_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Run the setup_postgresql.sh script
+./setup_postgresql.sh > lineage_setup.log 2>&1
+
+# Install npm packages
+npm install express pg >> lineage_setup.log 2>&1
+
+# Start the server as a background process
+nohup node index.js >> server.log 2>&1 &
