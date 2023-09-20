10c81c8dcfbc266997f04914bb3e9c1874cf3dd8
Samuel Snow <ssnow@suse.com>
2023-09-20 14:51:54 -0600

    Api endpoint (#9)
    * Added a Person Modal
    
    - A Modal for entering a person
    - The modal loads on the "/people" page when you select "add a person".
    - Includes radio inputs for "Sex" (M/F/?) and "Status" (Dead/Living)
    - Birth Date and Birthplace entry, and if "Dead", Death Date and DeathPlace.
    
    * Update package.json
    
    fixed missing react-modal and related packages.
    
    * Update PersonForm.js
    
    - Small fix for HandleStatusChange
    
    * fixes
    
    * Update PersonForm.js
    
    fix
    
    * updating FormInpput.css
    
    * Adding API handling for first addPerson
    
    This isn't quite working yet. However, you can run "http://192.168.0.45:3001/api/dbtest" and read the database, need to figure out why the submit button is still failing.
    
    * Update package-lock.json
    
    * Updates for api fetch addition
    
    - Made an api/addPerson
    - In the Person Modal, when you click submit, it runs the addPerson api
    
    * updates for api to db
    
    - some updates for importing data passed from api to backend for db import
    
    * update api db people imports
    
    - fixed api import problems
    
    * stuff I forgot to commit
    
    ---------
    
    Co-authored-by: root <blsnow98@gmail.com>

--- frontend/src/components/FormInput.js
+++ frontend/src/components/FormInput.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import './FormInput.css';
 
-export default function FormInput({label, type, options, onOptionChange}) {
+export default function FormInput({label, type, options, onChange}) {
+    const handleChange = (e) => {
+        if (onChange) {
+            onChange(e);
+        }
+    };
+
     if (type === 'radio' && options) {
-      return (
-        <div className="form-input">
-          <label>{label}</label>
-          <div>
-            {options.map((option, index) => (
-              <label key={index}>
-                <input type="radio" value={option} name={label}
-                  onChange={() => onOptionChange && onOptionChange(option)} />
-                {option}
-              </label>
-            ))}
-          </div>
-        </div>
-      );
+        return (
+            <div className="form-input">
+                <label>{label}</label>
+                <div>
+                    {options.map((option, index) => (
+                        <label key={index}>
+                            <input 
+                                type="radio" 
+                                value={option} 
+                                name={label}
+                                onChange={handleChange} 
+                            />
+                            {option}
+                        </label>
+                    ))}
+                </div>
+            </div>
+        );
     }
 
-  return (
-    <div className="form-group">
-      <label>{label}</label>
-      <input className="form-input" type={type} />
-    </div>
-  );
+    return (
+        <div className="form-group">
+            <label>{label}</label>
+            <input 
+                className="form-input" 
+                type={type} 
+                onChange={handleChange}
+            />
+        </div>
+    );
 }
